3b4e49922f37af6c55e992d45dd0d003021fe444
Adesina <omark@simplexsystem.com>
2021-07-08 08:49:30 +0100

    First commit

---
+++ CustomIdentity.Web/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomIdentity.Web.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("login")]
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
